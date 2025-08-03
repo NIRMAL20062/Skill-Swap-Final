e777859a51debf28fb17bf92454120beb8db2462
Nirmal K. <kumar.nirmal2608@gmail.com>
2025-08-03 09:39:33 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,17 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAhNE3IfnNeysORqHEf3hS6mInViTlxECU",
   authDomain: "skillswap-amrc7.firebaseapp.com",
   projectId: "skillswap-amrc7",
   storageBucket: "skillswap-amrc7.firebasestorage.app",
   messagingSenderId: "1050104005974",
-  appId: "1:1050104005974:web:84f0e293b97d48dd9582f7",
-  measurementId: ""
+  appId: "1:1050104005974:web:84f0e293b97d48dd9582f7"
 };
 
 // Initialize Firebase
